d7980959db288e3e569f2f9f74b46aeb4a278a2d
plainwalker <plainwalker@Proximo.localdomain>
2023-01-12 01:12:37 -0600

    19 Select Markets

--- src/componets/App.js
+++ src/componets/App.js
@@ -11,6 +11,7 @@ import {
 } from '../store/interactions';
 
 import Navbar from './Navbar'
+import Markets from './Markets'
 
 
 function App() {
@@ -51,7 +52,7 @@ function App() {
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
-          {/* Markets */}
+          <Markets />
 
           {/* Balance */}
 
