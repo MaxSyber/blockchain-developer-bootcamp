3375c371336c80c26d218524516c6733c03ca3d4
plainwalker <plainwalker@Proximo.localdomain>
2023-02-23 01:05:46 -0600

    29. Cancel Orders

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -51,6 +51,11 @@ export const loadExchange = async (provider, address, dispatch) => {
 }
 
 export const subscribeToEvents = (exchange, dispatch) => {
+	exchange.on('Cancel', (id, user, tokenGet, amountGet, tokenGive, amountGive, timestamp, event) =>{
+		const order = event.args
+		dispatch({ type: 'ORDER_CANCEL_SUCCESS', order, event })
+	})
+
 	exchange.on('Deposit', (token, user, amount, balance, event) =>{
 		dispatch({ type: 'TRANSFER_SUCCESS', event})
 	})
@@ -159,3 +164,14 @@ export const makeSellOrder = async (provider, exchange, tokens, order, dispatch)
 	}	
 }
 	
+export const cancelOrder = async (provider, exchange, order, dispatch) => {
+	dispatch({ type: 'ORDER_CANCEL_REQUEST' })
+
+	try {
+		const signer =await provider.getSigner()
+		const transaction = await exchange.connect(signer).cancelOrder(order.id)
+		await transaction.wait()
+	} catch (error) {
+		dispatch({ type: 'ORDER_CANCEL_FAIL' })
+	}
+}
